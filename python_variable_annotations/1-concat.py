63e69322e8598c6f082d8f081c11148f641834c0
DarianGrabino <dariangra@gmail.com>
2023-09-02 16:28:52 -0700

    function concatc with Variable Annotations

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,6 +1,7 @@
-#!/usr/bin/python3
-"""function concat string str1 and string str2,returns a concatenated string"""
+#!/usr/bin/env python3
+"""Write a type-annotated function concat"""
 
 
 def concat(str1: str, str2: str) -> str:
+    """function concat string str1 and string str2,returns a concatenated string"""
     return str1 + str2
