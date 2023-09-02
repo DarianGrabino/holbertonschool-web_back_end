dedbcd19dcaa0ac467d8d712d51d521b281a5859
DarianGrabino <dariangra@gmail.com>
2023-09-02 16:52:08 -0700

    function concatc with Variable Annotations

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -3,5 +3,6 @@
 
 
 def concat(str1: str, str2: str) -> str:
-    """function concat string str1 and string str2,returns a concatenated string"""
+    """function concat string str1 and string str2,
+    returns a concatenated string"""
     return str1 + str2
