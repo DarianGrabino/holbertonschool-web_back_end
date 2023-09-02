abbc358f5be136419df02e2300f49a775166bca7
DarianGrabino <dariangra@gmail.com>
2023-09-02 16:57:16 -0700

    returns a tuple int on the first element and square on the second

---
+++ python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""String and int/float to tuple"""
+from typing import Tuple, Union
+
+
+def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
+    """takes a string and an int or float, returns a tuple, 
+    int on the first element and square on the second"""
+    return (k, (v * v))
