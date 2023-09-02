826465f30d3053c922d4d64d4e29f1319b2b3a16
DarianGrabino <dariangra@gmail.com>
2023-09-02 16:57:25 -0700

    delete

--- 6-sum_mixed_list.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""Mixed list"""
-from typing import List, Union
-
-
-def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
-    """annotated function that takes a list of integers and floats
-     and returns their sum as a float"""
-    return sum(mxd_lst)
