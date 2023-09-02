e1f6767000f2deeda3b1cbf995eb1081d354417c
DarianGrabino <dariangra@gmail.com>
2023-09-02 16:33:53 -0700

    take a list and return its sum as a float with V. A.

--- python_variable_annotations/5-sum_list.py
+++ python_variable_annotations/5-sum_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """list of floats"""
+from typing import List
 
 
-def sum_list(input_list: 'list[float]') -> float:
+def sum_list(input_list: List[float]) -> float:
     """take a list of floats as an argument and return their sum as a float"""
     return sum(input_list)
