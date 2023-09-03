ead8b52b2747745acb2af86b2aa56f43dc679ba6
DarianGrabino <dariangra@gmail.com>
2023-09-03 13:22:33 -0700

    Type the parameters of the function

---
+++ python_variable_annotations/9-element_length.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Type the parameters of the function,
+ and return values with the appropriate types"""
+
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """return values with the appropriate types"""
+    return [(i, len(i)) for i in lst]
