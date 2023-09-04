85e600e9991c3353c43af936ffbdc1fdc9f04c49
DarianGrabino <dariangra@gmail.com>
2023-09-03 17:05:22 -0700

    list of all delays

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -1,16 +1,14 @@
 #!/usr/bin/env python3
 """Async function sintax"""
+import random
 import asyncio
 from typing import List
-wait_random = __import__('0-basic_async_syntax').wait_random
+task_wait_random = __import__('3-tasks').task_wait_random
 
 
-async def task_wait_n(n: int = 0, max_delay: int = 10) -> List[float]:
-    """Take wait_n and modify it into a new function task_wait_n""""
-    tasks = []
-    delays = []
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    """return a list of all delays"""
+    list_delays = []
     for i in range(n):
-        tasks.append(wait_random(max_delay))
-    for task in asyncio.as_completed(tasks):
-        delays.append(await task)
-    return delays
+        list_delays.append(await task_wait_random(max_delay))
+    return sorted(list_delays)
