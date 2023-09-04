ae4699d200f25142eacce1f9d9ca17b2d2939966
Darian <113607653+DarianGrabino@users.noreply.github.com>
2023-09-03 21:34:48 -0300

    Delete 1-async_comprehension.py

--- 1-async_comprehension.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-"""coroutine called async_comprehension"""
-from typing import List
-async_generator = __import__('0-async_generator').async_generator
-
-
-async def async_comprehension() -> List[float]:
-    """coroutine called async_comprehension"""
-    return [i async for i in async_generator()]
