621bdf07638d94f3dbc43a46c0518eedbf547f3e
DarianGrabino <dariangra@gmail.com>
2023-09-06 16:22:25 -0700

    change all topics based on the name

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -1,10 +1,18 @@
 #!/usr/bin/env python3
 """Change school topics"""
-from pymongo.collection import Collection
+import pymongo
 
 
-def update_topics(mongo_collection: Collection, name: str, topics: list):
-    """changes all topics of a school document based on the name"""
+def update_topics(mongo_collection, name, topics):
+    """
+    change all topics of a school document based on the name
+    Args:
+        mongo_collection: collection object
+        name (str) : name of the school to be updated
+        topics ([str]): list with topic names
+    Return:
+        number of modified documents
+    """
     filter_query = {"name": name}
     update_query = {"$set": {"topics": topics}}
     result = mongo_collection.update_many(filter_query, update_query)
