b7e5496903d79b17b6430ec044a0f88330da3eb7
DarianGrabino <dariangra@gmail.com>
2023-10-09 15:22:13 -0700

    promise with parameter booleans

--- ES6_promise/1-promise.js
+++ ES6_promise/1-promise.js
@@ -2,7 +2,7 @@ export default function getResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
     if (success) {
       resolve({
-        status: 200,body: 'Success',
+        status: 200, body: 'Success',
       });
     }
     reject(Error('The fake API is not working currently'));
